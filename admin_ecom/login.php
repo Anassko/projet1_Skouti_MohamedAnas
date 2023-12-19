ab87422eb3df82dbc084aafdd7655d884a54b2ce
Anas <medanas.skouti@gmail.com>
2023-12-19 03:33:29 -0500

    "connexion admin"

--- admin_ecom/login.php
+++ admin_ecom/login.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     exit();
                 } elseif ($_SESSION['user_role'] == 2) {
                     // Admin
-                    header("Location: admin.php");
+                    header("Location: index.php");
                     exit();
                 } elseif ($_SESSION['user_role'] == 3) {
                     // Regular user
