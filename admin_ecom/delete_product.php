7304c14c9751483fc5e31ac7c6cbdd56834990c1
Anas <medanas.skouti@gmail.com>
2023-12-19 02:36:14 -0500

    "style"

--- admin_ecom/delete_product.php
+++ admin_ecom/delete_product.php
@@ -33,10 +33,8 @@ $productResult = $con->query($productQuery);
 <html lang="en">
 
 <head>
-    <!-- MATERIAL DESIGN ICONIC FONT -->
+    
     <link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
-
-    <!-- STYLE CSS -->
     <link rel="stylesheet" href="../Rcss/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
--- admin_ecom/inserting_pro.php
+++ admin_ecom/inserting_pro.php
@@ -52,9 +52,9 @@ session_start();
 
 
 <head>
-    <!-- MATERIAL DESIGN ICONIC FONT -->
     <link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
     <link rel="stylesheet" href="../Rcss/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
    
 </head>
 <body>
