b0e322c1c4409b9d7cd794f6b39cc6123b59128a
Anas <medanas.skouti@gmail.com>
2023-12-20 01:13:17 -0500

    "comz"

--- admin_ecom/logout.php
+++ admin_ecom/logout.php
@@ -8,7 +8,7 @@ $_SESSION = array();
 // Destroy the session
 session_destroy();
 
-// Redirect to the login page or any other page after logout
+// Redirect to the login page 
 header("Location: login.php");
 exit();
 ?>
